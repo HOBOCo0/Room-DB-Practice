974324125a3beff4fef015103d12a4c635e75459
aman1 <aman15220@gmail.com>
2024-07-17 14:19:40 +0530

    create Dao interface

---
+++ app/src/main/java/com/example/roomdatabasepractice/ContactDAO.kt
@@ -0,0 +1,34 @@
+package com.example.roomdatabasepractice
+
+import androidx.lifecycle.LiveData
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.Query
+import androidx.room.Update
+
+/*
+  -> We declare all the methods to access the data here. all the implementation
+     of these methods are provided by room.
+
+  -> We execute these functions on the background thread because Android don't
+     want to execute long running tasks on main thread or ui thread so we make these functions
+     as suspend as we want coroutine to run these functions.
+
+  -> We can make threads manually or we can use executors to run these functions.
+ */
+@Dao
+interface ContactDAO {
+    @Insert
+    suspend fun insertContact(contact:Contact)
+    @Update
+    suspend fun updateContact(contact:Contact)
+    @Delete
+    suspend fun deleteContact(contact:Contact)
+
+    // for querry we will use live data, Behind the scene Rood checks if its return type is LiveData
+    // then by default it is executed on background thread. here we don't have to manually declare
+    // it to be a suspend function
+    @Query("SELECT * FROM contact")
+    suspend fun getContact(): LiveData<List<Contact>>
+}
